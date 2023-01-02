038e2454863e2964ad5aeb6974e2c775b87d749c
LAPTOP-3K2C8ERM\toren <toren@toren.uk>
2023-01-02 13:09:19 +0000

    feat: test websocket for widgets

--- index.ts
+++ index.ts
@@ -5,9 +5,22 @@ import helmet from 'helmet'
 import cors from 'cors'
 import routes from '@routes/index.route'
 import configureMongoose from '@db/configure.mongoose'
+import http from 'http'
+import {Server} from 'socket.io'
 import {handleError} from '@middleware/handle.error'
 
 const app = express()
+const server = http.createServer(app)
+const io = new Server(server, {cors: {origin: '*'}})
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  socket.emit('message', 'hello from moolah backend')
+})
+
+io.on('message', (msg) => {
+  console.log(msg)
+})
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
@@ -25,3 +38,5 @@ app.use(handleError)
 app.listen(config.port, () => {
   console.info(`server started on port ${config.port} (${config.env})`)
 })
+// socket.io server
+server.listen(3333)
